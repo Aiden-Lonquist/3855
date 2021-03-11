6fd75b5082be9b7453f8e47f2e5256c7d01e45b8
Aiden-Lonquist <55508988+Aiden-Lonquist@users.noreply.github.com>
2021-03-11 10:49:13 -0800

    Update app.py

--- storage/app.py
+++ storage/app.py
@@ -57,7 +57,6 @@ def get_custom_order(timestamp):
 
 def process_messages():
     """ Process event messages """
-    logger.info("starting message process service")
     hostname = "%s:%d" % (app_config["events"]["hostname"], app_config["events"]["port"])
     client = KafkaClient(hosts=hostname)
     topic = client.topics[str.encode(app_config["events"]["topic"])]
